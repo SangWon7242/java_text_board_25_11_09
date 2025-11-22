01362d896862efc7041614ca3636ee4a4c3b7422
SangWon7242 <gotkdqja@gmail.com>
2025-11-22 13:17:19 +0900

    중복 된 아이디로 로그인 금지

--- src/main/java/com/sbs/domain/member/controller/MemberController.java
+++ src/main/java/com/sbs/domain/member/controller/MemberController.java
@@ -6,9 +6,6 @@ import com.sbs.global.base.container.Container;
 import com.sbs.global.base.controller.BaseController;
 import com.sbs.global.base.rq.Rq;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MemberController implements BaseController  {
   private MemberService memberService;
 
@@ -28,6 +25,7 @@ public class MemberController implements BaseController  {
     String password;
     String passwordConfirm;
     String name;
+    Member oldMember;
 
     System.out.println("== 회원 가입 ==");
 
@@ -35,6 +33,13 @@ public class MemberController implements BaseController  {
     while (true) {
       System.out.print("아이디 : ");
       username = Container.sc.nextLine();
+
+      oldMember = memberService.findByUsername(username);
+
+      if(oldMember != null) {
+        System.out.println("이미 존재하는 아이디입니다.");
+        continue;
+      }
 
       if(username.trim().isEmpty()) {
         System.out.println("아이디를 입력해주세요.");
--- src/main/java/com/sbs/domain/member/service/MemberService.java
+++ src/main/java/com/sbs/domain/member/service/MemberService.java
@@ -14,4 +14,8 @@ public class MemberService {
   public Member join(String username, String password, String name) {
     return memberRepository.join(username, password, name);
   }
+
+  public Member findByUsername(String username) {
+    return memberRepository.findByUsername(username);
+  }
 }
