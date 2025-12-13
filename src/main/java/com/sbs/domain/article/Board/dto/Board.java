ee36767872fa9ac6a8172db957de8c8caa1e0c7e
SangWon7242 <gotkdqja@gmail.com>
2025-12-13 13:30:33 +0900

    Article 클래스에 boardId 추가

--- src/main/java/com/sbs/domain/article/Board/dto/Board.java
+++ src/main/java/com/sbs/domain/article/Board/dto/Board.java
@@ -17,7 +17,7 @@ public class Board {
     lastId = 0;
   }
 
-  public Board(String title, String content, int memberId, String writerName) {
-    this(++lastId, title, content, memberId, writerName);
+  public Board(String name, String code) {
+    this(++lastId, name, code);
   }
 }
